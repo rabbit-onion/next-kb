523358e5878aadaa00e4c15686ecb72a0cb5f129
Onion <csr1000@naver.com>
2024-11-28 17:50:27 +0900

    section layout

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,14 @@
+import { ChevronRight } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className='pb-96 flex flex-col gap-4'>
       <section>
-        <div>
-          <div className='text-[18vw] font-bold'>thinK Great</div>
+        <div className='container'>
+          <div className='text-[16vw] font-bold'>thinK Great</div>
           <div className='relative'>
             {/* Image의 width, height는 렌더링 사이즈임 */}
             <Image src='/images/intro.png' alt='' width={1300} height={1300} className='w-full h-[400px] object-cover' />
@@ -20,6 +22,37 @@ const AboutPage = () => {
                 만나다
               </strong>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className='container'>
+          <ul>
+            <li>
+              <Link href='#' className='flex flex-col gap-3'>
+                <Image src='/images/cat.jpg' alt='' width={1300} height={1300} className='w-full aspect-auto object-cover' />
+                <div className='flex flex-col'>
+                  <span className='text-base text-gray-500'>소셜미디어</span>
+                  <strong className='text-xl'>소통과 화합의 현장!KG모빌리티 파트너스 데이</strong>
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section>
+        <div className='container'>
+          <h2>
+            <Link href='#'>
+              곽재선의 창 <ChevronRight />
+            </Link>
+          </h2>
+          <div>
+            <Link href='#'>
+              <Image src='/images/chair.jpg' alt='' width={1300} height={1300} />
+            </Link>
           </div>
         </div>
       </section>
